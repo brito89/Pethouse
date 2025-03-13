ec4ed32aecf9d3a6d8df7be79818b34045358ac3
Juan Brito <cocobrito3@gmail.com>
2025-03-13 13:41:02 -0600

    Add data seeder

---
+++ PethouseAPI/Data/Seed/DataSeeder.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PethouseAPI.Data.Seed;
+
+public class DataSeeder
+{
+    private readonly ApplicationDbContext _context;
+
+    public DataSeeder(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Migrate()
+    {
+        await _context.Database.MigrateAsync();
+    }
+}
